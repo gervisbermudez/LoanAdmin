0534b8bae986f58fcb2ce444b0644d6a203909f9
Gervis Bermudez <gervisbermudez@outlook.com>
2018-10-07 23:16:34 -0300

    Upgrate siteversion

--- application/config/constants.php
+++ application/config/constants.php
@@ -93,4 +93,4 @@ defined('IMGPATH')             OR define('IMGPATH', PUBLICPATH.'img/');
 defined('IMGPROFILEPATH')      OR define('IMGPROFILEPATH', IMGPATH.'profile/');
 
 //Define the currente site version 
-defined('SITEVERSION')      OR define('SITEVERSION', '1.0');
+defined('SITEVERSION')      OR define('SITEVERSION', '1.1');
