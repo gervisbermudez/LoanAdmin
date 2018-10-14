7fbac40b2c59c424fef516b41c6284c6c1bfe399
Gervis Bermudez <gervisbermudez@outlook.com>
2018-10-14 11:44:42 -0300

    Add print_r_this function on helper

--- application/helpers/functions_helper.php
+++ application/helpers/functions_helper.php
@@ -1,7 +1,6 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-if ( ! function_exists('fnAddScript'))
-{
+defined('BASEPATH') or exit('No direct script access allowed');
+if (!function_exists('fnAddScript')) {
 	/**
 	 * Generate a script link with the base url based on the filepath
 	 * @param  [type] $strFilePath [description]
@@ -9,13 +8,21 @@ if ( ! function_exists('fnAddScript'))
 	 */
 	function fnAddScript($strFilePath, $attr = array())
 	{
-		if(strpos($strFilePath, 'http') === false){
+		if (strpos($strFilePath, 'http') === false) {
 			$strFilePath = base_url($strFilePath);
 		}
 		$linkattr = '';
 		foreach ($attr as $key => $value) {
-			$linkattr .= $key.'="'.$value.'" '; 
+			$linkattr .= $key . '="' . $value . '" ';
 		}
-		return '<script src="'.$strFilePath.'" '.$linkattr.'></script>';			
+		return '<script src="' . $strFilePath . '" ' . $linkattr . '></script>';
+	}
+}
+if (!function_exists('print_r_this')) {
+	function print_r_this($var)
+	{
+		echo '<pre>';
+		print_r($var);
+		echo '</pre>';
 	}
 }
