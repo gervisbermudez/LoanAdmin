e6efcf10bb0cdcf2972c07136ba0cc34fc6fa51f
Gervis Bermudez <gervisbermudez@outlook.com>
2018-10-20 17:14:49 -0300

    Herramientas en todos los clientes

--- application/views/admin/prestamo/cliente_page.php
+++ application/views/admin/prestamo/cliente_page.php
@@ -20,9 +20,12 @@ $modalid = random_string('alnum', 16);
 <div class="box">
   <div class="box-header with-border">
     <h3 class="box-title">Herramientas</h3>
-    <div class="box-tools pull-right">
-    	<button class="btn btn-box-tool"><i class="fa fa-table"></i></button>
-    </div>
+    <hr />
+					<a href="<?php echo base_url('admin/prestamo/nuevo'); ?>" class="btn btn-success" data-toggle="tooltip"
+           data-original-title="Nuevo prestamo"><i class="fa fa-plus-circle"></i> Registrar prestamo</a>
+           <?php if ($this->session->userdata('user')['level'] < 2) : ?>
+                  <a role="menuitem" data-toggle="modal" data-target="<?= '#' . $modalid ?>" tabindex="-1" href="#!" data-table-reference="clients" data-value-id="<?= $cliente['id']; ?>" data-target="" data-delete-redirect="true" data-delete-redirectto="admin/prestamo/clientes" class="delete-data btn btn-danger"><i class="fa fa-close"></i> Eliminar</a>
+            <?php endif ?>
   </div>
 <!-- /.box-footer-->
 </div>
@@ -41,15 +44,15 @@ $modalid = random_string('alnum', 16);
                   Opciones <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu">
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo base_url('admin/prestamo/nuevo/user/' . $cliente['id']); ?>">Registrar Prestamo</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="<?= base_url('admin/prestamo/nuevo/user/' . $cliente['id']); ?>">Registrar Prestamo</a></li>
                   <?php if ($this->session->userdata('user')['level'] < 2) : ?>
                   <li role="presentation" class="divider"></li>
-                  <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo base_url('admin/prestamo/cliente/editar/' . $cliente['id']); ?>">Editar</a></li>
-                  <li role="presentation"><a role="menuitem" data-toggle="modal" data-target="<?php echo '#' . $modalid ?>" tabindex="-1" href="#!" data-table-reference="clients" data-value-id="<?php echo $cliente['id']; ?>" data-target="" data-delete-redirect="true" data-delete-redirectto="admin/prestamo/clientes" class="delete-data">Eliminar</a></li>
+                  <li role="presentation"><a role="menuitem" tabindex="-1" href="<?= base_url('admin/prestamo/cliente/editar/' . $cliente['id']); ?>">Editar cliente</a></li>
+                  <li role="presentation"><a role="menuitem" data-toggle="modal" data-target="<?= '#' . $modalid ?>" tabindex="-1" href="#!" data-table-reference="clients" data-value-id="<?= $cliente['id']; ?>" data-target="" data-delete-redirect="true" data-delete-redirectto="admin/prestamo/clientes" class="delete-data">Eliminar cliente</a></li>
                   <?php endif ?>
                 </ul>
               </li>
-              <li class="pull-left header"><i class="fa fa-user"></i> <?php echo $cliente['nombre'] . ' ' . $cliente['apellido'] ?></li>
+              <li class="pull-left header"><i class="fa fa-user"></i> <?= $cliente['nombre'] . ' ' . $cliente['apellido'] ?></li>
             </ul>
             <div class="tab-content">
             <div class="tab-pane" id="tab_calendar">
@@ -83,15 +86,15 @@ $modalid = random_string('alnum', 16);
                  <?php foreach ($historial_prestamo as $key => $prestamo) : ?>
                    <tr>
                     <?php if ($this->session->userdata('user')['level'] < 3) : ?>
-                    <td><a href="<?php echo base_url('admin/user/view/' . $prestamo['id_prestamista']); ?>"><?php echo $prestamo['username'] ?></a></td>
+                    <td><a href="<?= base_url('admin/user/view/' . $prestamo['id_prestamista']); ?>"><?= $prestamo['username'] ?></a></td>
                   <?php endif ?>
-                     <td><?php echo $prestamo['monto'] ?> $&nbsp;<a title='Ver Cuotas' href="<?php echo base_url('admin/prestamo/cuotas/' . $prestamo['id']); ?>"><i class="fa fa-fw fa-search-plus"></i></a></td>
-                     <td><?php echo $prestamo['porcentaje'] ?>%</td>
-                     <td><?php echo $prestamo['monto_total'] ?> $</td>
-                     <td><?php echo $prestamo['cant_cuotas'] ?></td>
-                     <td><?php echo $prestamo['ciclo_pago'] ?></td>
-                     <td><?php echo $prestamo['fecha_inicio'] ?></td>
-                     <td><span class="badge bg-green"><?php echo $prestamo['progreso'] ?>%</span></td>
+                     <td><?= $prestamo['monto'] ?> $&nbsp;<a title='Ver Cuotas' href="<?= base_url('admin/prestamo/cuotas/' . $prestamo['id']); ?>"><i class="fa fa-fw fa-search-plus"></i></a></td>
+                     <td><?= $prestamo['porcentaje'] ?>%</td>
+                     <td><?= $prestamo['monto_total'] ?> $</td>
+                     <td><?= $prestamo['cant_cuotas'] ?></td>
+                     <td><?= $prestamo['ciclo_pago'] ?></td>
+                     <td><?= $prestamo['fecha_inicio'] ?></td>
+                     <td><span class="badge bg-green"><?= $prestamo['progreso'] ?>%</span></td>
                    </tr>
                  <?php endforeach ?>
                 </tbody>
@@ -138,14 +141,14 @@ $modalid = random_string('alnum', 16);
                  <?php foreach ($prestamos as $key => $prestamo) : ?>
                    <tr>
                     <?php if ($this->session->userdata('user')['level'] < 3) : ?>
-                    <td><a href="<?php echo base_url('admin/user/view/' . $prestamo['id_prestamista']); ?>"><?php echo $prestamo['username'] ?></a></td>
+                    <td><a href="<?= base_url('admin/user/view/' . $prestamo['id_prestamista']); ?>"><?= $prestamo['username'] ?></a></td>
                     <?php endif ?>
-                     <td><?php echo $prestamo['monto'] ?> $&nbsp;<a title='Ver Cuotas' href="<?php echo base_url('admin/prestamo/cuotas/' . $prestamo['id']); ?>"><i class="fa fa-fw fa-search-plus"></i></a></td>
-                     <td><?php echo $prestamo['porcentaje'] ?>%</td>
-                     <td><?php echo $prestamo['monto_total'] ?> $</td>
-                     <td><?php echo $prestamo['cant_cuotas'] ?></td>
-                     <td><?php echo $prestamo['ciclo_pago'] ?></td>
-                     <td><?php echo $prestamo['fecha_inicio'] ?></td>
+                     <td><?= $prestamo['monto'] ?> $&nbsp;<a title='Ver Cuotas' href="<?= base_url('admin/prestamo/cuotas/' . $prestamo['id']); ?>"><i class="fa fa-fw fa-search-plus"></i></a></td>
+                     <td><?= $prestamo['porcentaje'] ?>%</td>
+                     <td><?= $prestamo['monto_total'] ?> $</td>
+                     <td><?= $prestamo['cant_cuotas'] ?></td>
+                     <td><?= $prestamo['ciclo_pago'] ?></td>
+                     <td><?= $prestamo['fecha_inicio'] ?></td>
                    </tr>
                  <?php endforeach ?>
                 </tbody>
@@ -174,24 +177,24 @@ $modalid = random_string('alnum', 16);
                   <div class="col-lg-8">
                     <div class="row">
                       <div class="col-lg-4">
-                        <div class="box <?php echo $boxColor ?>">
+                        <div class="box <?= $boxColor ?>">
                           <div class="box-header with-border">
                             <h3 class="box-title">Información</h3>
                           </div>
                           <div class="box-body">
                             <strong><i class="fa fa-phone margin-r-5"></i> Teléfono</strong>
-                            <p class="text-muted"><a href="tel:<?php echo $cliente['telefono'] ?>"><?php echo $cliente['telefono'] ?></a></p>
+                            <p class="text-muted"><a href="tel:<?= $cliente['telefono'] ?>"><?= $cliente['telefono'] ?></a></p>
                             <hr>
                             <strong><i class="fa fa-map-marker margin-r-5"></i> Location</strong>
-                            <p class="text-muted"><a href="https://www.google.com.ar/maps/search/<?php echo urlencode($cliente['direccion']) ?>" target="_blank"><?php echo $cliente['direccion'] ?></a></p>
+                            <p class="text-muted"><a href="https://www.google.com.ar/maps/search/<?= urlencode($cliente['direccion']) ?>" target="_blank"><?= ucwords($cliente['direccion']) ?></a></p>
                             <hr>
                             <strong><i class="fa fa-calendar-plus-o margin-r-5"></i> Fecha de registro</strong>
-                            <p class="text-muted"><?php echo $cliente['registerdate'] ?></p>
+                            <p class="text-muted"><?= $cliente['registerdate'] ?></p>
                           </div>
                         </div>
                         </div>
                         <div class="col-lg-8">
-                          <div class="box <?php echo $boxColor ?>">
+                          <div class="box <?= $boxColor ?>">
                             <div class="box-header with-border">
                               <h3 class="box-title">Prestamos Activos</h3>
                             </div>
@@ -226,24 +229,24 @@ $modalid = random_string('alnum', 16);
                                   ?>
                                   <tr>
                                     <td>
-                                      <?php echo $prestamo['ciclo_pago'] ?>
+                                      <?= $prestamo['ciclo_pago'] ?>
                                     </td>
                                     <td>
-                                      <?php echo $prestamo['monto'] ?>$&nbsp;<a title='Ver Cuotas' href="<?php echo base_url('admin/prestamo/cuotas/' . $prestamo['id']); ?>"><i class="fa fa-fw fa-search-plus"></i></a>
+                                      <?= $prestamo['monto'] ?>$&nbsp;<a title='Ver Cuotas' href="<?= base_url('admin/prestamo/cuotas/' . $prestamo['id']); ?>"><i class="fa fa-fw fa-search-plus"></i></a>
                                     </td>
                                     <td>
                                     <div class="progress progress-xs progress-striped active">
-                                      <div class="progress-bar <?php echo $progresstyle ?>" style="width: <?php echo $prestamo['progreso'] ?>%"></div>
+                                      <div class="progress-bar <?= $progresstyle ?>" style="width: <?= $prestamo['progreso'] ?>%"></div>
                                     </div>
                                   </td>
-                                  <td><span class="badge <?php echo $badge ?>"><?php echo $progreso ?>%</span></td>
+                                  <td><span class="badge <?= $badge ?>"><?= $progreso ?>%</span></td>
                                   </tr>  
                                 <?php endforeach ?>
                               </tbody>
                             </table>
                             </div>
                           <?php else : ?>
-                              <div class="box-body">No hay prestamos activos, <a href="<?php echo base_url('admin/prestamo/nuevo/user/' . $cliente['id']); ?>">Registrar nuevo</a></div>
+                              <div class="box-body">No hay prestamos activos, <a href="<?= base_url('admin/prestamo/nuevo/user/' . $cliente['id']); ?>">Registrar nuevo</a></div>
                           <?php endif ?>
                             <!-- /.box-body -->
                           </div>
@@ -254,22 +257,22 @@ $modalid = random_string('alnum', 16);
                   <div class="box box-widget widget-user-2">
                       <!-- Add the bg color to the header using any of the bg-* classes -->
                       
-                      <div class="widget-user-header <?php echo $badge; ?>">
+                      <div class="widget-user-header <?= $badge; ?>">
                         <!-- /.widget-user-image -->
                         <h3 class="widget-user-username">Balance Global</h3>
                         <?php if((int)$balance['porcentaje_total_pagado']===100): ?>
                         <h5 class="widget-user-desc">Sin deuda</h5>
                         <?php else: ?>
-                        <h5 class="widget-user-desc"><?php echo (int)$balance['porcentaje_total_pagado'] ?>% Pagado</h5>
+                        <h5 class="widget-user-desc"><?= (int)$balance['porcentaje_total_pagado'] ?>% Pagado</h5>
                         <?php endif; ?>
                       </div>
                       <div class="box-footer no-padding">
                         <ul class="nav nav-stacked">
-                          <li><a href="#"><b>Monto Prestado</b> <span class="pull-right"><?php echo number_format($balance['monto_total_prestado'], 2, ',', '.') ?>$</span></a></li>
-                          <li><a href="#"><b>Monto Ganado</b> <span class="pull-right"><?php echo number_format($balance['monto_total_ganado'], 2, ',', '.') ?>$</span></a></li>
-                          <li><a href="#"><b>Monto total</b> <span class="pull-right"><?php echo number_format($balance['monto_gobal_prestado'], 2, ',', '.') ?>$</span></a></li>
-                          <li><a href="#"><b>Total Pagado</b> <span class="pull-right"><?php echo number_format($balance['monto_total_pagado'], 2, ',', '.') ?>$</span></a></li>
-                          <li><a href="#"><b>Deuda actual</b> <span class="pull-right"><?php echo number_format($balance['monto_deuda_total'], 2, ',', '.') ?>$</span></a></li>
+                          <li><a href="#"><b>Monto Prestado</b> <span class="pull-right"><?= number_format($balance['monto_total_prestado'], 2, ',', '.') ?>$</span></a></li>
+                          <li><a href="#"><b>Monto Ganado</b> <span class="pull-right"><?= number_format($balance['monto_total_ganado'], 2, ',', '.') ?>$</span></a></li>
+                          <li><a href="#"><b>Monto total</b> <span class="pull-right"><?= number_format($balance['monto_gobal_prestado'], 2, ',', '.') ?>$</span></a></li>
+                          <li><a href="#"><b>Total Pagado</b> <span class="pull-right"><?= number_format($balance['monto_total_pagado'], 2, ',', '.') ?>$</span></a></li>
+                          <li><a href="#"><b>Deuda actual</b> <span class="pull-right"><?= number_format($balance['monto_deuda_total'], 2, ',', '.') ?>$</span></a></li>
                         </ul>
                       </div>
                     </div>
@@ -310,7 +313,7 @@ $modalid = random_string('alnum', 16);
   </div>
   <!-- /.col -->
 </div>
-<div class="modal fade" id="<?php echo $modalid; ?>" style="display: none;">
+<div class="modal fade" id="<?= $modalid; ?>" style="display: none;">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
@@ -362,10 +365,10 @@ foreach ($pagos_realizados as $key => $value) : ?>
       }
       ?>
       {
-        title          : 'Cuota de <?php echo $cuota["monto_total"] ?>',
-        start          : new Date(<?php echo $fecha_pago->format('Y') . ', ' . ((int)($fecha_pago->format('m')) - 1) . ', ' . $fecha_pago->format('d') . ',08,00,00' ?>),
-        backgroundColor: '<?php echo $Eventcolor ?>', //red
-        borderColor    : '<?php echo $Eventcolor ?>', //red
+        title          : 'Cuota de <?= $cuota["monto_total"] ?>',
+        start          : new Date(<?= $fecha_pago->format('Y') . ', ' . ((int)($fecha_pago->format('m')) - 1) . ', ' . $fecha_pago->format('d') . ',08,00,00' ?>),
+        backgroundColor: '<?= $Eventcolor ?>', //red
+        borderColor    : '<?= $Eventcolor ?>', //red
         url            : '<?= base_url() . 'admin/prestamo/cuotas/' . $cuota["id_prestamo"] ?>',
       },
     <?php endforeach ?>
